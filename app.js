f19a5d07c7ecd65d26060adf29baafa1fdbe5a8a
Theophilus Boakye <93124888+TheophilusBoakye@users.noreply.github.com>
2022-05-30 12:50:52 +0000

    add security packages

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 require("express-async-errors");
 const express = require("express");
 const app = express();
+
 // routes
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
@@ -12,7 +19,17 @@ const authenticationMiddleware = require("./middleware/authentication");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+	rateLimiter({
+		windowMs: 15 * 60 * 1000, // 15 minutes
+		max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	})
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 // extra packages
 
 // routes
